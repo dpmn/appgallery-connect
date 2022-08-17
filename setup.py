1ca808611e488b32e69188e3983980ca1e65b8cf
Oleg Ionov <ionov.dpmn@gmail.com>
2022-08-17 11:35:52 +0300

    Initial commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="appgallery-connect",
-    version="0.0.1",
+    version="0.0.2",
     description="DPMN tap for extracting data",
     author="",
     url="",
